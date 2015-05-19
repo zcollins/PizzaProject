dd0793f9ecef65f8d4020030ef3b09bb6fadd17c
Zac Collins <zcollins@petrichor.io>
2015-05-19 19:14:58 -0400

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
+<html>
+    <body>
+         <form action="<?php echo $_GET["name"]; ?>" method="get">
+             Name: <input type="text" name="name"><br>
+             E-mail: <input type="text" name="email"><br>
+             <input type="submit">
+         </form>
+    </body>
+</html>
 
-echo "Test";
+
