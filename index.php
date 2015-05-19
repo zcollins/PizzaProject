2935632db9bbff44944c3973e6389ceddcfc6bc9
Zac Collins <zcollins@petrichor.io>
2015-05-19 19:45:36 -0400

    first commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <input type="text" name="<?php $_POST['$order']?>>
             <br>
             <br><br>
-            <input type="submit" value="Orderup!">
+            <input type="submit" value="Submit">
         </form>
     </body>
 </html>
