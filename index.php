ef7f7694d1c151149bd6ff940b29bad000bcbb3f
Zac Collins <zcollins@petrichor.io>
2015-05-19 19:51:03 -0400

    first commit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
     <body>
-        <form action="<?php echo"$order"?>" method="POST">
+        <form action="<?php echo"$myString"?>" method="POST">
             Order<br>
-            <input type="text" name="order">
+            <input type="text" name="<?php $myString?>>
             <br><br>
             <input type="submit" value="Submit">
         </form>
