7b9447ed60bdfd23952547ea523f64b942c06930
Zac Collins <zcollins@petrichor.io>
2015-05-19 19:56:12 -0400

    first commit

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<html>
-    <body>
-        <form action=<?php echo"$myString"?>>
-            Order<br>
-            <input type="text" name=<?php $myString?>
-            <br><br>
-            <input type="submit" value="Submit">
-        </form>
-    </body>
-</html>
+<?php
+// This won't work because of the quotes around specialH5!
+echo "<h5 class="specialH5">I love using PHP!</h5>";
+
+// OK because we escaped the quotes!
+echo "<h5 class=\"specialH5\">I love using PHP!</h5>";
+
+// OK because we used an apostrophe '
+echo "<h5 class='specialH5'>I love using PHP!</h5>";
+?>
