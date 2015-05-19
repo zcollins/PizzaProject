df0c19baa4e3cb57607f1eac0298ff829c2cefee
Zac Collins <zcollins@petrichor.io>
2015-05-19 19:57:42 -0400

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <html>
-<h1>this</h1>
+<?php
+// This won't work because of the quotes around specialH5!
+echo "<h5 class="specialH5">I love using PHP!</h5>";
+
+// OK because we escaped the quotes!
+echo "<h5 class=\"specialH5\">I love using PHP!</h5>";
+
+// OK because we used an apostrophe '
+echo "<h5 class='specialH5'>I love using PHP!</h5>";
+?>
 </html>
