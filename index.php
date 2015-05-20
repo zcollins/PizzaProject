97152db7854624432949e72a77cfef39e02eb630
Zac Collins <zcollins@petrichor.io>
2015-05-19 21:37:41 -0400

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+
+/* checkboxes for toppings,
+fill out the rest
+Look into reading and writing files*/
 <!DOCTYPE html>
 <html>
 <body>
@@ -7,38 +11,46 @@
         Order<br>
         <label for="size">Crust Size</label>
         <select  name="size">
+            <option value="small">Medium</option>
             <option value="medium">Medium</option>
             <option value="large">Large</option>
         </select>
         <br />
-        <input type="text" name="type" placeholder="cheese or bbq">
+        <input type="checkbox" name="toppings" value="pepperoni">Pepperoni<br />
+        <input type="checkbox" name="toppings" value="bbq">BBQ<br />
+        <input type="checkbox" name="toppings" value="Hawaiian">Hawaiian<br />
+        <input type="checkbox" name="toppings" value="steak">Steak<br />
         <br />
-        <input type="deliverySpeed" name="deliverySpeed" placeholder="by bicycle or jetpack?">
-        <input type="submit" value="Order">
+
+        <label for="delivery">Delivery Method</label>
+        <select  name="delivery">
+            <option value="train">Train</option>
+            <option value="plane">Medium</option>
+            <option value="automobile">Large</option>
+        </select>
     </form>
-<?php } else { ?>
-    <?php
+<?php } else {
+    $pricing = array(
+        'sizes' => array(
+            'small' => 5,
+            'medium' => 10,
+            'large' => 15
+        ),
+        'toppings' => array(
+            'pepperoni' => 9,
+            'bbq' => 10,
+            'Hawaiian' => 5,
+            'steak' => 16
+        ),
+        'deliveryMethods' => array(
+            'train' => 10,
+            'plane' => 20,
+            'automobile' => 5
+        )
+    );
+
     $total = 0;
-    if($_POST['size']=='medium') {
-       $total+= $total+10;
-    }
-    elseif($_POST['size']=='large')    {
-        $total+= $total+15;
-    };
-    if($_POST['type']=='cheese')    {
-        $total+= $total+4;
-    }
-    elseif($_POST['type']=='bbq')    {
-        $total+= $total+5;
-    }
-    if($_POST['deliverySpeed']=='bicycle')   {
-        $total+= $total+10;
-    }
-    elseif($_POST['deliverySpeed']=='jetpack')    {
-        $total+= $total+35000;
-    };
-        echo $total;
-    ?>
-<?php } ?>
+    echo $total;
+} ?>
 </body>
 </html>
