42004bd89d41eb54a0f558e578a0fb65c3524670
Zac Collins <zcollins@petrichor.io>
2015-05-19 20:10:54 -0400

    first commit

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<!DOCTYPE html>
 <html>
-    <input type="text" name=$firstname>
-        <?php
-        $myString = "Hello!";
-        echo $firstname;
-        ?>
-        <br><br>
+<body>
+
+<form action="action_page.php">
+    Order<br>
+    <input type="text" name="order" value="Order?">
+    <br><br>
     <input type="submit" value="Submit">
+    <?php $GET["name"] ?>
+</form>
+
+
+</body>
 </html>
