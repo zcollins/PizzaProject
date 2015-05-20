54d572ea5441fd0460039b346b10088bf132d961
Zac Collins <zcollins@petrichor.io>
2015-05-19 20:30:48 -0400

    first commit

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html>
 <body>
 
-<form action="" method="POST">
-    Order<br>
-    <input type="text" name="order" placeholder="Order?">
-    <br><br>
-    <input type="submit" value="Submit">
-    <?php $_POST["$order"] ?>
-    <?php echo $_POST['order'] ?>
-</form>
-
+<?php if(empty($_POST)) { ?>
+    <form action="" method="POST">
+        Order<br>
+        <input type="text" name="order" placeholder="Order?">
+        <br><br>
+        <input type="submit" value="Submit">
+    </form>
+<?php } else { ?>
+    <?=print_r($_POST);?>
+<?php } ?>
 </body>
 </html>
